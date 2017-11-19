ca84df54095ff41264ab3f655683d653759eb101
Nico <ndubouilh@gmail.com>
2017-11-19 23:23:37 +0100

    ADD strstr, strnstr | UPDATE strcmp, strncmp | NOT FINISH strlcat | OK

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,26 +6,20 @@
 /*   By: ndubouil <ndubouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 14:52:51 by ndubouil          #+#    #+#             */
-/*   Updated: 2017/11/16 15:24:27 by ndubouil         ###   ########.fr       */
+/*   Updated: 2017/11/19 21:23:42 by ndubouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-	size_t i;
+{	
+		size_t i;
 
-	i = 0;
-	while (s1[i] != '\0' && n > i)
-	{
-		if (s2[i] == '\0' || s2[i] < s1[i])
-			return (1);
-		if (s2[i] > s1[i])
-			return (-1);
-		i++;
-	}
-	if (s2[i] != '\0')
-		return (-1);
-	return (0);
+		i = 0;
+		while (s1[i] == s2[i] && i < (n - 1))
+			i++;
+		if (n != 0)
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		return (0);
 }
--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: ndubouil <ndubouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 11:32:49 by ndubouil          #+#    #+#             */
-/*   Updated: 2017/11/16 11:36:27 by ndubouil         ###   ########.fr       */
+/*   Updated: 2017/11/19 22:51:31 by ndubouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 char	*ft_strstr(const char *haystack, const char *needle)
 {
-	return ("blabla");
+	int i;
+	int j;
+
+	if (ft_strlen(needle) == 0)
+		return ((char *)haystack);
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j])
+		{
+			j++;
+			if (needle[j] == '\0')
+			return ((char *)&haystack[i]);
+		}
+		i++;
+	}
+	return (0);
 }
