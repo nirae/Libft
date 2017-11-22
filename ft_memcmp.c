7e657559f36c16f0f7f77bc227097e313da098c8
Nico <ndubouilh@gmail.com>
2017-11-22 20:32:26 +0100

    ALL TESTS PASSED | NEED TO bonus | OK

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: ndubouil <ndubouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/21 12:33:54 by ndubouil          #+#    #+#             */
-/*   Updated: 2017/11/22 18:28:38 by ndubouil         ###   ########.fr       */
+/*   Updated: 2017/11/22 20:05:54 by ndubouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,11 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n)
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	i = 0;
-	while (str1[i] == str2[i] && i < (n - 1))
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
 		i++;
-	if (n != 0)
-		return (str1[i] - str2[i]);
+	}
 	return (0);
 }
